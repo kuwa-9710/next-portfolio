9585330d224484dc31cdbe898fd3de8575213136
tomoya <shima.apple0941@gmail.com>
2023-03-29 18:35:30 +0900

    Pathを追加

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,22 @@ import styles from "@/components/Header/Header.module.css";
 
 export function Header() {
   return (
-    <header className="flex justify-center md:justify-between items-center h-16 border-b-2 px-8">
-      <span className="block font-bold text-3xl text-slate-700 text-center md:text-start">PortFolio</span>
+    <header className="flex justify-center md:justify-between items-center h-16 px-8">
+      <span className="block font-bold text-3xl text-white text-center md:text-start">PortFolio</span>
       <nav className="gap-6 items-center hidden md:flex">
-        <Link className="text-slate-500" href="/">
+        <Link className="text-white" href="/">
+          Home
+        </Link>
+        <Link className="text-white" href="/about">
           About Me
         </Link>
-        <Link className="text-slate-500" href="/">
+        <Link className="text-white" href="/news">
           News
         </Link>
-        <Link className="text-slate-500" href="/">
+        <Link className="text-white" href="/">
           Works
         </Link>
-        <Link className="text-slate-500" href="/">
+        <Link className="text-white" href="/">
           Contact
         </Link>
       </nav>
