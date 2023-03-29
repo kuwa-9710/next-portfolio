a55c9fddc3843e5f64e487e866c8a410bd2bde96
tomoya <shima.apple0941@gmail.com>
2023-03-29 21:43:36 +0900

    背景を追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export function Main(props) {
-  return <main className="h-screen w-screen">{props.children}</main>;
+  return (
+  <main className="h-screen w-screen">
+    {props.children}
+    <Image className="absolute top-0 left-0 -z-10 w-full h-full object-cover" src={`/bg.jpg`} alt="background image" width={1000} height={1000} />
+  </main>
+
+  )
 }
