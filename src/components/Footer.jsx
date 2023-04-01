790074c0447f7d76dd828c4a55a3ab3b6bcc3b7d
tomoya <shima.apple0941@gmail.com>
2023-04-01 17:39:35 +0900

    Footerを追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+import { Links } from "./Links";
+
 export function Footer() {
   return (
     <>
-      <footer className="flex justify-center py-4 border-t-1">
-        <small className="text-slate-700 text-center">
+      <footer className="flex justify-center py-12 border-t-1 flex-col bg-slate-800">
+        <Link href={"/"} className="text-center"><span className="text-center text-3xl font-bold text-white">Portfolio</span></Link>
+        <nav className="text-center mt-4">
+          <ul className="flex flex-col gap-6">
+            <Links />
+          </ul>
+        </nav>
+        <small className="text-white text-center mt-4">
           2023 Tomoya all rights reserved.
         </small>
       </footer>
