1ed03a125355d6f977c4484bcee139d7667909bc
tomoya <shima.apple0941@gmail.com>
2023-04-01 15:10:07 +0900

    z-indexを調整

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "@/components/Header/Header.module.css";
 
 export function Header() {
   return (
-    <header className="absolute w-full flex justify-center md:justify-between items-center h-16 px-8">
+    <header className="absolute z-50 w-full flex justify-center md:justify-between items-center h-16 px-8">
       <span className="block font-bold text-3xl text-white">PortFolio</span>
       <nav className="gap-6 items-center hidden md:flex">
         <Link className="text-white active:text-sky-400 hover:text-sky-400" href="/">
