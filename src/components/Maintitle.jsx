97eae7358168b0d6e049a69b72dcceb7077f5d99
tomoya <shima.apple0941@gmail.com>
2023-04-01 14:59:33 +0900

    テキストカラーを変更

--- src/components/Maintitle.jsx
+++ src/components/Maintitle.jsx
@@ -1,7 +1,8 @@
 export const Maintitle = () => {
   return (
     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center">
-      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-sky-500 from-fuchsia-600 drop-shadow-md">      
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br
+    from-sky-400 via-blue-700 to-purple-700 drop-shadow-md">      
         Hi! I&apos;m Tomoya.
       </h1>
       <p className="mt-4 text-white drop-shadow-sm">Web Developer in Sendai.</p>
