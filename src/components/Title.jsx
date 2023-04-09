ebbea7ec146c4025132699b6cbeedbf424fbfbe9
tomoya <shima.apple0941@gmail.com>
2023-04-09 22:00:18 +0900

    デザインを修正

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 export const Title = (props) => {
   return (
-    <h2 className="text-4xl md:text-6xl font-bold text-white drop-shadow-md mt-6 leading-[150%] md:leading-[150%]">
+    <h2 className="text-4xl md:text-6xl font-bold text-white drop-shadow-md mt-8 md:mt-16 leading-[150%] md:leading-[150%]">
       {props.title}
     </h2>
   );
