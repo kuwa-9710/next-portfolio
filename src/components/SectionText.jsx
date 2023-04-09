168336385711266e561a7bdcca353ff806f03119
tomoya <shima.apple0941@gmail.com>
2023-04-09 22:00:34 +0900

    デザインを修正

--- src/components/SectionText.jsx
+++ src/components/SectionText.jsx
@@ -1,6 +1,6 @@
 export const SectionText = (props) => {
   return (
-    <p className="text-slate-500 text-md md:text-2xl mt-6 leading-loose md:leading-[150%]">
+    <p className="text-slate-500 text-md md:text-2xl mt-16 leading-loose md:leading-[150%]">
       {props.title}
     </p>
   );
