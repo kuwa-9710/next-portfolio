9347ada4aa8d0fd1f136e5ac76c6835570aa10cd
tomoya <shima.apple0941@gmail.com>
2023-04-10 09:31:16 +0900

    全体的な修正

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import { Subtitle } from "./Subtitle";
 import { Title } from "./Title";
 import { WorksContent } from "./WorksContent";
 
-export const Works = ({ works }) => {  
+export const Works = ({ works }) => {
   return (
     <div className="w-full mx-auto max-w-5xl px-4 mt-32 md:mt-56">
       <Subtitle title="My Works" />
       <Title title="美しく、使いやすく、効果的になるよう心掛けています" />
       <WorksContent works={works} />
+      <Link
+        className="mt-6 mx-auto flex items-center w-full max-w-xs justify-center gap-4 py-4 px-12 
+          bg-slate-800 text-white rounded-lg active:bg-sky-500 hover:bg-sky-400
+          transition-all"
+        href={"/work"}
+      >
+        view all works
+      </Link>
     </div>
   );
 };
