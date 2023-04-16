b1a5e8e8b851dc45b62310aaa363db5aee59a19b
tomoya <shima.apple0941@gmail.com>
2023-04-16 10:27:27 +0900

    bg-fixedをやめました。

--- src/components/Mv.jsx
+++ src/components/Mv.jsx
@@ -1,6 +1,9 @@
 export const Mv = (props) => {
   return (
-    <div id="mv" className="relative w-full h-full min-h-screen flex flex-col justify-center">
+    <div
+      id="mv"
+      className="relative w-full h-full min-h-screen flex flex-col justify-center -z-10 bg-[url('/bg.jpg')] bg-cover "
+    >
       {props.children}
     </div>
   );
