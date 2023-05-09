59a164338f547be79dcf1a89d891329b27106c1a
tomoya <shima.apple0941@gmail.com>
2023-05-09 21:30:19 +0900

    デザインを修正

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,33 +47,6 @@ export function Header() {
           <span></span>
         </button>
 
-        {/* <button
-        className="block lg:hidden cursor-pointer focus:outline-none z-50"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <svg
-          className={`w-6 h-6 fill-current ${
-            isOpen ? "text-white" : "text-white"
-          }`}
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {isOpen ? (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M3.585 3.585a1 1 0 0 1 1.414 0L10 8.586l4.243-4.243a1 1 0 1 1 1.414 1.414L11.414 10l4.243 4.243a1 1 0 1 1-1.414 1.414L10 11.414l-4.243 4.243a1 1 0 0 1-1.414-1.414L8.586 10 4.343 5.757a1 1 0 0 1 0-1.414z"
-            />
-          ) : (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M2 4.5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm0 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm1 5a1 1 0 1 1 0-2h14a1 1 0 1 1 0 2H3z"
-            />
-          )}
-        </svg>
-      </button> */}
-
         <nav
           className={`fixed top-0 right-0 h-screen w-[300px] bg-slate-800 z-40 transition-transform duration-700 transform ${
             isActive ? "translate-x-0" : "translate-x-[150%]"
