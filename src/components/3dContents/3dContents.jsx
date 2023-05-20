ddb4527bab9d6f81665c7bb820157acf90804f88
tomoya <shima.apple0941@gmail.com>
2023-05-20 23:33:46 +0900

    3dコンテンツの欄を追加

---
+++ src/components/3dContents/3dContents.jsx
@@ -0,0 +1,14 @@
+import style from "./3dContent.module.scss";
+import { Subtitle } from "../Subtitle/Subtitle";
+
+export const Contents_3D = () => {
+  return (
+    <>
+      <div className={style.container}>
+        <Subtitle title="3D-Contents" />
+        <span className={style.title}>Comming Soon!</span>
+        <p className={style.text}>3Dコンテンツは準備中です。Githubからソースコードを閲覧することができます。</p>
+      </div>
+    </>
+  );
+};
