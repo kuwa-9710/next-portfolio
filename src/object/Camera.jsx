08a40544ff91d204aa22c34be829d7cabb3e0e13
tomoya <shima.apple0941@gmail.com>
2023-06-15 22:58:56 +0900

    cameraを動かすように変更

--- src/object/Camera.jsx
+++ src/object/Camera.jsx
@@ -1,5 +1,6 @@
 import { useThree, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { Vector3 } from "three";
 
 export function Camera(props) {
   const { camera } = useThree();
@@ -20,8 +21,19 @@ export function Camera(props) {
     }
   };
 
+  const orbitCenter = new Vector3(150, 0, 0); // 中心位置
+  const radius = 400; // 周回半径
+  const angleSpeed = 0.2; // 周回速度Ï
+
   useFrame(() => {
     updateCameraPosition();
+
+    const theta = angleSpeed * performance.now() * 0.001; // 現在の時間に基づく角度
+    const x = orbitCenter.x + radius * Math.cos(theta);
+    const y = orbitCenter.y + radius * Math.cos(theta);
+    const z = orbitCenter.z + radius * Math.sin(theta);
+    camera.position.set(x, y, z);
+    camera.lookAt(orbitCenter);
   });
 
   return null;
